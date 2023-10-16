970db0d746723e590f5a339ebe546bb5568ca2cc
allankruger <allanfloreskruger@gmail.com>
2023-10-16 18:26:59 +0200

    feat: add header part of Player component

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,3 +1,23 @@
+import { MessageCircle } from "lucide-react";
+
 export function Player() {
-  return <div>Player</div>;
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+      <div className="flex w-[1100px] flex-col gap-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-1 ">
+            <h1 className="text-2xl font-bold">Redux Fundamentals</h1>
+            <span className="text-sm text-zinc-400">Unraveling Redux</span>
+          </div>
+
+          <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
+            <MessageCircle className="w-4 h-4" />
+            Leave feedback
+          </button>
+        </div>
+
+        <main></main>
+      </div>
+    </div>
+  );
 }
