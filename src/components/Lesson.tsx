1787c1622a78278bd27ff87bc10637a80449ab9c
allankruger <allanfloreskruger@gmail.com>
2023-10-17 07:00:57 +0200

    feat: add hover effect to Lesson button

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -7,7 +7,7 @@ interface LessonProps {
 
 export function Lesson({ title, duration }: LessonProps) {
   return (
-    <button className="flex items-center gap-3 text-sm text-zinc-400 border-zinc-800 border-b-[1px] pb-2 last:border-b-0">
+    <button className="flex items-center gap-3 text-sm text-zinc-400 border-zinc-800 border-b-[1px] pb-2 last:border-b-0 hover:text-slate-50">
       <Video className="w-4 h-4 text-zinc-500" />
       <span>{title}</span>
       <span className="ml-auto font-mono text-xs text-zinc-500">
